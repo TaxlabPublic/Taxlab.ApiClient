60fdff23ba71cfac88e3511ebbd1d15e005727dd
Kenji Cheung <kenji@taxlab.online>
2022-06-21 16:29:22 +1200

    Added test for tax payments

---
+++ src/TaxLab.Test.ApiClientCli/TaxpayerDomain/TaxPayments/TaxPaymentTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using NodaTime;
+using Taxlab.ApiClientCli.Implementations;
+using Taxlab.ApiClientCli.Repositories.Taxpayer;
+using Taxlab.ApiClientLibrary;
+using Xunit;
+
+namespace TaxLab.Test.ApiClientCli.TaxpayerDomain.TaxPayments
+{
+    public class TaxPaymentTest
+    {
+        [Fact]
+        public async void CreateTaxPayment()
+        {
+            string baseUrl = "https://preview.taxlab.online/api-internal/";
+            HttpClient httpclient = new HttpClient();
+            var authService = new AuthService();
+            TaxlabApiClient client = new TaxlabApiClient(baseUrl, httpclient, authService);
+
+            var repo = new TaxPaymentRepository(client);
+            var taxYear = 2021;
+
+            var taxpayerId = Guid.NewGuid();
+
+            var taxPayment = await repo.CreateAsync(taxpayerId,
+                taxYear,
+                "PAYG Installment",
+                5250, // PAYG ID
+                true,
+                new LocalDate(2021, 1, 1),
+                -1000);
+        }
+    }
+}
---
+++ src/Taxlab.ApiClientCli/Repositories/Taxpayer/TaxPaymentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using NodaTime;
+using TaxLab;
+using Taxlab.ApiClientCli.Workpapers.Shared;
+using Taxlab.ApiClientLibrary;
+
+namespace Taxlab.ApiClientCli.Repositories.Taxpayer
+{
+    public class TaxPaymentRepository : RepositoryBase
+    {
+        public TaxPaymentRepository(TaxlabApiClient client) : base(client)
+        {
+        }
+
+        public async Task<TaxPaymentsResponse> CreateAsync(
+            Guid taxpayerId,
+            int taxYear = 0,
+            string description = "description",
+            int transactionTypeId = 0,
+            bool applyToTaxableIncome = true,
+            LocalDate Date = new LocalDate(),
+            decimal amount = 0
+        )
+        {
+            var newTaxReturnCommand = new UpsertTaxPaymentCommand()
+            {
+                TaxpayerId = taxpayerId,
+                TaxYear = taxYear,
+                Description = description,
+                TransactionTypeId = transactionTypeId,
+                ApplyToTaxableIncome = applyToTaxableIncome,
+                Date = Date.ToAtoDateString(),
+                Amount = amount
+            };
+
+            var taxReturnResponse = await Client.Taxpayers_PutTaxPaymentsAsync(newTaxReturnCommand).ConfigureAwait(false);
+
+            return taxReturnResponse;
+        }
+    }
+}
