ff37435b2a53111cf9d246c0a4c45d2a448c8f1f
Danesh Balasingam <danesh.balasingam@taxlab.online>
2022-06-22 12:07:00 +1200

    testpush

--- src/Taxlab.ApiClientLibrary/Proxy/TaxlabApiClient.cs
+++ src/Taxlab.ApiClientLibrary/Proxy/TaxlabApiClient.cs
@@ -20,7 +20,7 @@ namespace Taxlab.ApiClientLibrary
     [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.15.10.0 (NJsonSchema v10.6.10.0 (Newtonsoft.Json v11.0.0.0))")]
     public partial interface ITaxlabApiClient
     {
-
+        ///testing
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <exception cref="ApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<FileResponse> Echo_GetAsync(string taxpayerId = null, int? taxYear = null, double? version = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
